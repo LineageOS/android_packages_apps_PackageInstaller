e76495778be7d8e2ed9fc7c8ba45361dd8ba5705
Bryan Owens <djbryan3540@gmail.com>
2016-04-08 19:41:40 -0500

    PackageInstaller: Add tint mode to icons
    This allows custom icons can show in permissions settings
    
    Change-Id: Idfcb64ec8ae53e4f51000e6c792e23c5da4a6641
    Signed-off-by: Bryan Owens <djbryan3540@gmail.com>

--- src/com/android/packageinstaller/permission/utils/Utils.java
+++ src/com/android/packageinstaller/permission/utils/Utils.java
@@ -25,6 +25,7 @@ import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.content.res.Resources.Theme;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
 import android.util.ArraySet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -112,6 +113,7 @@ public final class Utils {
         theme.resolveAttribute(attr, typedValue, true);
         icon = icon.mutate();
         icon.setTint(context.getColor(typedValue.resourceId));
+        icon.setTintMode(PorterDuff.Mode.SRC_ATOP);
         return icon;
     }
 
