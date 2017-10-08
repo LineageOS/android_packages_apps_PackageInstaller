f7716d2ff0ed0ae1bc215e8be692cb9c7043502f
Patrick Young <kmahyygyyg@gmail.com>
2017-10-08 03:28:47 +0000

    Revert "DO NOT MERGE Disable overlays while installer is resumed"
    This reverts commit cec631f26e2cbd7c51fd7c28e05d379d6d0f86db.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Icd0d1528e0b5ff8bf01dccc701a6ccb17656df7e

--- src/com/android/packageinstaller/PackageInstallerActivity.java
+++ src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -16,6 +16,7 @@
 */
 package com.android.packageinstaller;
 
+import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -52,8 +53,6 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import com.android.packageinstaller.permission.utils.Utils;
 
-import com.android.packageinstaller.permission.ui.OverlayTouchActivity;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -70,8 +69,7 @@ import java.io.OutputStream;
  * Based on the user response the package is then installed by launching InstallAppConfirm
  * sub activity. All state transitions are handled in this activity
  */
-public class PackageInstallerActivity extends OverlayTouchActivity implements OnCancelListener,
-        OnClickListener {
+public class PackageInstallerActivity extends Activity implements OnCancelListener, OnClickListener {
     private static final String TAG = "PackageInstaller";
 
     private static final int REQUEST_ENABLE_UNKNOWN_SOURCES = 1;
@@ -217,25 +215,6 @@ public class PackageInstallerActivity extends OverlayTouchActivity implements On
                     mOkCanInstall = true;
                 }
             });
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mOk != null) {
-            mOk.setEnabled(mOkCanInstall);
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mOk != null) {
-            // Don't allow the install button to be clicked as there might be overlays
-            mOk.setEnabled(false);
         }
     }
 
