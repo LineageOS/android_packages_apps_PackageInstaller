LineageOS/android_packages_apps_PackageInstaller

commit cec631f26e2cbd7c51fd7c28e05d379d6d0f86db

DO NOT MERGE Disable overlays while installer is resumed
The install button can be disabled for other reasons, hence we have to
store the enabled state independent of the resumed/paused state.

Test: gts-tradefed run gts-dev -m PackageInstallerTapjacking

Bug: 35056974
Change-Id: I2effa0f5afacfaed217a030550a778e32912cfbb
(cherry picked from commit 172eddd2ff07d9849496de586b3a785d8abc4441)

src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -16,7 +16,6 @@
 */
 package com.android.packageinstaller;
 
-import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -53,6 +52,8 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import com.android.packageinstaller.permission.utils.Utils;
 
+import com.android.packageinstaller.permission.ui.OverlayTouchActivity;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -69,7 +70,8 @@ import java.io.OutputStream;
  * Based on the user response the package is then installed by launching InstallAppConfirm
  * sub activity. All state transitions are handled in this activity
  */
-public class PackageInstallerActivity extends Activity implements OnCancelListener, OnClickListener {
+public class PackageInstallerActivity extends OverlayTouchActivity implements OnCancelListener,
+        OnClickListener {
     private static final String TAG = "PackageInstaller";
 
     private static final int REQUEST_ENABLE_UNKNOWN_SOURCES = 1;
@@ -215,6 +217,25 @@ public class PackageInstallerActivity extends Activity implements OnCancelListen
                     mOkCanInstall = true;
                 }
             });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mOk != null) {
+            mOk.setEnabled(mOkCanInstall);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mOk != null) {
+            // Don't allow the install button to be clicked as there might be overlays
+            mOk.setEnabled(false);
         }
     }
 

commit cad5e648fb66b2683d21f01681b1f25f3f640684

Don't disable the install button permanently
The button is enabled only from onCreate(), but mOkCanInstall is
true only if the list has been fully scrolled. If mOkCanInstall is
false when onResume() is called, the button gets disabled and
never re-enabled, even if mOkCanInstall becomes true. Since the
install button can be used to scroll the list when mOkCanInstall
is false, add a separate flag so that we can re-enable the button
even if mOkCanInstall is false.

Fixes: cec631f26e2c ("DO NOT MERGE Disable overlays while installer is resumed")

REGRESSION-1262

Change-Id: I819aab95506672d291e07193b71f074292862754

src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -106,6 +106,7 @@ public class PackageInstallerActivity extends OverlayTouchActivity implements On
     private Button mCancel;
     CaffeinatedScrollView mScrollView = null;
     private boolean mOkCanInstall = false;
+    private boolean mOkEnabled;
 
     static final String PREFS_ALLOWED_SOURCES = "allowed_sources";
 
@@ -225,7 +226,7 @@ public class PackageInstallerActivity extends OverlayTouchActivity implements On
         super.onResume();
 
         if (mOk != null) {
-            mOk.setEnabled(mOkCanInstall);
+            mOk.setEnabled(mOkEnabled);
         }
     }
 
@@ -235,6 +236,7 @@ public class PackageInstallerActivity extends OverlayTouchActivity implements On
 
         if (mOk != null) {
             // Don't allow the install button to be clicked as there might be overlays
+            mOkEnabled = mOk.isEnabled();
             mOk.setEnabled(false);
         }
     }